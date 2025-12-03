9e1c8801e4e75971bcee8da9144924a58ea32065
Lavan Sellar <lavansellar@gmail.com>
2025-12-03 12:18:11 +0100

    Correction page 403

---
+++ src/main/java/com/nnk/springboot/controllers/CustomErrorController.java
@@ -0,0 +1,26 @@
+package com.nnk.springboot.controllers;
+
+import jakarta.servlet.RequestDispatcher;
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.boot.web.servlet.error.ErrorController;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class CustomErrorController implements ErrorController {
+
+    @RequestMapping("/error")
+    public String handleError(HttpServletRequest request) {
+        Object status = request.getAttribute(RequestDispatcher.ERROR_STATUS_CODE);
+
+        if (status != null) {
+            int statusCode = Integer.parseInt(status.toString());
+
+            if (statusCode == 403) {
+                return "403";
+            }
+        }
+
+        return "error/default";
+    }
+}
